962f69e2941cd9ce4d944dcbf532ed5b5bfdfc57
Scott Mackenzie <Scott.Mackenzie@myfoodbag.co.nz>
2019-08-01 14:34:33 +1200

    test harness

---
+++ ServiceBus.EnvPluginTests/EnvPluginTests.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Xunit;
+
+namespace ServiceBus.EnvPluginTests
+{
+    public class EnvPluginTests
+    {
+        [Fact]
+        public async Task ShouldStampMessage()
+        {
+            var message = new Message(null);
+            var env = "Development";
+
+            var plugin = new EnvPlugin.EnvPlugin(env);
+            var result = await plugin.BeforeMessageSend(message);
+
+            Assert.Null(result.Body);
+            Assert.True(message.UserProperties.ContainsKey("Env"), "Header should have env stamped.");
+        }
+    }
+}
